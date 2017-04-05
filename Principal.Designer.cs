9728601d546d32ebc5dca21ffb1f4af081664c13
desarrolloutec <desarrollo.software@utec-celaya.edu.mx>
2017-04-04 19:01:58 -0500

    Adding code
    Adding first version

---
+++ Principal.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Desocultator
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.configuracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bloqueosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mBloqueaNoExtraibles = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuracionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.terminalInformativaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mLimite25 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mLimite50 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mLimite100 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gbAccion = new System.Windows.Forms.GroupBox();
+            this.btnRecuperar = new System.Windows.Forms.Button();
+            this.btnMostrarArchivos = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.cmbDrives = new System.Windows.Forms.ComboBox();
+            this.gbArchivos = new System.Windows.Forms.GroupBox();
+            this.listFiles = new System.Windows.Forms.ListView();
+            this.chNombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chAtributos = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.gbTerminal = new System.Windows.Forms.GroupBox();
+            this.txtConsola = new System.Windows.Forms.TextBox();
+            this.menuPrincipal.SuspendLayout();
+            this.gbAccion.SuspendLayout();
+            this.gbArchivos.SuspendLayout();
+            this.gbTerminal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuracionToolStripMenuItem,
+            this.ayudaToolStripMenuItem});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(542, 24);
+            this.menuPrincipal.TabIndex = 0;
+            this.menuPrincipal.Text = "menu";
+            // 
+            // configuracionToolStripMenuItem
+            // 
+            this.configuracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bloqueosToolStripMenuItem,
+            this.configuracionToolStripMenuItem1});
+            this.configuracionToolStripMenuItem.Name = "configuracionToolStripMenuItem";
+            this.configuracionToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.configuracionToolStripMenuItem.Text = "Opciones";
+            // 
+            // bloqueosToolStripMenuItem
+            // 
+            this.bloqueosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mBloqueaNoExtraibles});
+            this.bloqueosToolStripMenuItem.Name = "bloqueosToolStripMenuItem";
+            this.bloqueosToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.bloqueosToolStripMenuItem.Text = "Bloqueos";
+            // 
+            // mBloqueaNoExtraibles
+            // 
+            this.mBloqueaNoExtraibles.Checked = true;
+            this.mBloqueaNoExtraibles.CheckOnClick = true;
+            this.mBloqueaNoExtraibles.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.mBloqueaNoExtraibles.Name = "mBloqueaNoExtraibles";
+            this.mBloqueaNoExtraibles.Size = new System.Drawing.Size(241, 22);
+            this.mBloqueaNoExtraibles.Text = "Bloquear unidades no extraibles";
+            this.mBloqueaNoExtraibles.Click += new System.EventHandler(this.mBloqueaNoExtraibles_Click);
+            // 
+            // configuracionToolStripMenuItem1
+            // 
+            this.configuracionToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.terminalInformativaToolStripMenuItem});
+            this.configuracionToolStripMenuItem1.Name = "configuracionToolStripMenuItem1";
+            this.configuracionToolStripMenuItem1.Size = new System.Drawing.Size(150, 22);
+            this.configuracionToolStripMenuItem1.Text = "Configuracion";
+            // 
+            // terminalInformativaToolStripMenuItem
+            // 
+            this.terminalInformativaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mLimite25,
+            this.mLimite50,
+            this.mLimite100});
+            this.terminalInformativaToolStripMenuItem.Name = "terminalInformativaToolStripMenuItem";
+            this.terminalInformativaToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.terminalInformativaToolStripMenuItem.Text = "Terminal informativa";
+            // 
+            // mLimite25
+            // 
+            this.mLimite25.Name = "mLimite25";
+            this.mLimite25.Size = new System.Drawing.Size(125, 22);
+            this.mLimite25.Text = "25 lineas";
+            this.mLimite25.Click += new System.EventHandler(this.aumentarLimiteLineasTerminal);
+            // 
+            // mLimite50
+            // 
+            this.mLimite50.Name = "mLimite50";
+            this.mLimite50.Size = new System.Drawing.Size(125, 22);
+            this.mLimite50.Text = "50 lineas";
+            this.mLimite50.Click += new System.EventHandler(this.aumentarLimiteLineasTerminal);
+            // 
+            // mLimite100
+            // 
+            this.mLimite100.Name = "mLimite100";
+            this.mLimite100.Size = new System.Drawing.Size(125, 22);
+            this.mLimite100.Text = "100 lineas";
+            this.mLimite100.Click += new System.EventHandler(this.aumentarLimiteLineasTerminal);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.acercaDeToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // gbAccion
+            // 
+            this.gbAccion.Controls.Add(this.btnRecuperar);
+            this.gbAccion.Controls.Add(this.btnMostrarArchivos);
+            this.gbAccion.Controls.Add(this.btnActualizar);
+            this.gbAccion.Controls.Add(this.cmbDrives);
+            this.gbAccion.Location = new System.Drawing.Point(12, 27);
+            this.gbAccion.Name = "gbAccion";
+            this.gbAccion.Size = new System.Drawing.Size(172, 79);
+            this.gbAccion.TabIndex = 1;
+            this.gbAccion.TabStop = false;
+            this.gbAccion.Text = "Accion";
+            // 
+            // btnRecuperar
+            // 
+            this.btnRecuperar.Location = new System.Drawing.Point(88, 47);
+            this.btnRecuperar.Name = "btnRecuperar";
+            this.btnRecuperar.Size = new System.Drawing.Size(75, 23);
+            this.btnRecuperar.TabIndex = 3;
+            this.btnRecuperar.Text = "Recuperar";
+            this.btnRecuperar.UseVisualStyleBackColor = true;
+            this.btnRecuperar.Click += new System.EventHandler(this.btnRecuperar_Click);
+            // 
+            // btnMostrarArchivos
+            // 
+            this.btnMostrarArchivos.Location = new System.Drawing.Point(7, 47);
+            this.btnMostrarArchivos.Name = "btnMostrarArchivos";
+            this.btnMostrarArchivos.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrarArchivos.TabIndex = 2;
+            this.btnMostrarArchivos.Text = "Mostrar unidad";
+            this.btnMostrarArchivos.UseVisualStyleBackColor = true;
+            this.btnMostrarArchivos.Click += new System.EventHandler(this.btnMostrarArchivos_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Image = global::Desocultator.Properties.Resources.refresh_small;
+            this.btnActualizar.Location = new System.Drawing.Point(127, 20);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(36, 23);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // cmbDrives
+            // 
+            this.cmbDrives.FormattingEnabled = true;
+            this.cmbDrives.Location = new System.Drawing.Point(7, 20);
+            this.cmbDrives.Name = "cmbDrives";
+            this.cmbDrives.Size = new System.Drawing.Size(114, 21);
+            this.cmbDrives.TabIndex = 0;
+            // 
+            // gbArchivos
+            // 
+            this.gbArchivos.Controls.Add(this.listFiles);
+            this.gbArchivos.Location = new System.Drawing.Point(190, 27);
+            this.gbArchivos.Name = "gbArchivos";
+            this.gbArchivos.Size = new System.Drawing.Size(341, 206);
+            this.gbArchivos.TabIndex = 2;
+            this.gbArchivos.TabStop = false;
+            this.gbArchivos.Text = "Contenido de la unidad";
+            // 
+            // listFiles
+            // 
+            this.listFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chNombre,
+            this.chAtributos});
+            this.listFiles.Location = new System.Drawing.Point(7, 20);
+            this.listFiles.Name = "listFiles";
+            this.listFiles.Size = new System.Drawing.Size(328, 180);
+            this.listFiles.TabIndex = 0;
+            this.listFiles.UseCompatibleStateImageBehavior = false;
+            this.listFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // chNombre
+            // 
+            this.chNombre.Text = "Nombre";
+            this.chNombre.Width = 187;
+            // 
+            // chAtributos
+            // 
+            this.chAtributos.Text = "Atributos";
+            this.chAtributos.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.chAtributos.Width = 137;
+            // 
+            // gbTerminal
+            // 
+            this.gbTerminal.Controls.Add(this.txtConsola);
+            this.gbTerminal.Location = new System.Drawing.Point(12, 242);
+            this.gbTerminal.Name = "gbTerminal";
+            this.gbTerminal.Size = new System.Drawing.Size(519, 100);
+            this.gbTerminal.TabIndex = 3;
+            this.gbTerminal.TabStop = false;
+            this.gbTerminal.Text = "Terminal informativa";
+            // 
+            // txtConsola
+            // 
+            this.txtConsola.BackColor = System.Drawing.Color.Black;
+            this.txtConsola.ForeColor = System.Drawing.Color.Lime;
+            this.txtConsola.Location = new System.Drawing.Point(7, 20);
+            this.txtConsola.Multiline = true;
+            this.txtConsola.Name = "txtConsola";
+            this.txtConsola.Size = new System.Drawing.Size(511, 74);
+            this.txtConsola.TabIndex = 0;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 354);
+            this.Controls.Add(this.gbTerminal);
+            this.Controls.Add(this.gbArchivos);
+            this.Controls.Add(this.gbAccion);
+            this.Controls.Add(this.menuPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuPrincipal;
+            this.Name = "Principal";
+            this.Text = "Desocultator";
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.gbAccion.ResumeLayout(false);
+            this.gbArchivos.ResumeLayout(false);
+            this.gbTerminal.ResumeLayout(false);
+            this.gbTerminal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem configuracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bloqueosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mBloqueaNoExtraibles;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.GroupBox gbAccion;
+        private System.Windows.Forms.Button btnRecuperar;
+        private System.Windows.Forms.Button btnMostrarArchivos;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.ComboBox cmbDrives;
+        private System.Windows.Forms.GroupBox gbArchivos;
+        private System.Windows.Forms.ListView listFiles;
+        private System.Windows.Forms.ColumnHeader chNombre;
+        private System.Windows.Forms.ColumnHeader chAtributos;
+        private System.Windows.Forms.GroupBox gbTerminal;
+        private System.Windows.Forms.TextBox txtConsola;
+        private System.Windows.Forms.ToolStripMenuItem configuracionToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem terminalInformativaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mLimite25;
+        private System.Windows.Forms.ToolStripMenuItem mLimite50;
+        private System.Windows.Forms.ToolStripMenuItem mLimite100;
+    }
+}
+
---
+++ Principal.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desocultator
+{
+    public partial class Principal : Form
+    {
+        public bool bloqueaHD = true;
+        public int limiteLineasConsola = 25;
+
+        public Principal()
+        {
+            InitializeComponent();
+            buscaUnidadesDrive();
+            Consola("Bienvenido");
+        }
+
+        private void Consola(string msj)
+        {
+            if (txtConsola.Lines.Count() > limiteLineasConsola)
+                txtConsola.Clear();
+
+            string simbolo = (bloqueaHD) ? "$" : "#";
+            txtConsola.AppendText(simbolo + " " + msj + "\r\n");
+            gbTerminal.Text = "Terminal Informativa: mostrando " + (txtConsola.Lines.Count() - 1) + " lineas" +
+                " de " + limiteLineasConsola;
+        }
+
+        public String ejecutarComando(string comando)
+        {
+            Consola("ejecutando: " + comando);
+            listFiles.Items.Clear();
+            DriveInfo unidad = new DriveInfo(cmbDrives.Text);
+
+            if (unidad.DriveType == DriveType.Fixed && bloqueaHD == true)
+            {
+                string msj = "Por seguridad, esta opcion ha sido bloqueada\nen unidades no extraibles.";
+                MessageBox.Show(msj);
+                return msj;
+            }
+
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + comando);
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+            procStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            return (result.Length > 1) ? result : "comando ejecutado";
+        }
+
+        /**
+         * Busca unidades drive conectadas al equipo y actualiza la lista
+         */
+        private void buscaUnidadesDrive()
+        {
+            cmbDrives.Items.Clear();
+            IEnumerable<DriveInfo> unidadesDrive = DriveInfo.GetDrives().Where(c => c.DriveType == DriveType.Fixed || c.DriveType == DriveType.Removable);
+            foreach (DriveInfo d in unidadesDrive)
+                if (d.IsReady == true)
+                    cmbDrives.Items.Add(d.Name);
+            cmbDrives.SelectedIndex = 0;
+            Consola("Unidades de almacenamiento cargadas");
+        }
+
+        private void buscaArchivos(string ruta)
+        {
+            listFiles.Items.Clear();
+            DriveInfo unidad = new DriveInfo(ruta);
+
+            if (unidad.DriveType == DriveType.Fixed && bloqueaHD == true)
+            {
+                string msj = "La unidad que elegiste no parece ser una memoria USB, ¿deseas continuar?";
+                DialogResult op = MessageBox.Show(msj, "Unidad NO extraible", MessageBoxButtons.YesNo);
+                Consola(msj);
+                if (op == DialogResult.No)
+                    return;
+            }
+
+            /*
+            DriveInfo drive= new DriveInfo(ruta);
+            string msj = "";
+            msj += "Espacio disponible: "+ drive.AvailableFreeSpace + "\n";
+            msj += "Formato: "+ drive.DriveFormat + "\n";
+            msj += "Tipo: "+ drive.DriveType + "\n";
+            msj += "Nombre: "+ drive.Name + "\n";
+            msj += "Raíz: "+ drive.RootDirectory + "\n";
+            msj += "Espacio total disponible: "+ drive.TotalFreeSpace + "\n";
+            msj += "Tamaño total:"+ drive.TotalSize + "\n";
+            msj += "Etiqueta: "+ drive.VolumeLabel + "\n";
+            MessageBox.Show(msj);
+            */
+
+            ListViewItem item;
+            Color color;
+            foreach (DirectoryInfo f in unidad.RootDirectory.GetDirectories())
+            {
+                color = Color.Black;
+                item = new ListViewItem(f.Name);
+                item.SubItems.Add(f.Attributes.ToString());
+                if ((f.Attributes & FileAttributes.System) == FileAttributes.System)
+                {
+                    color = Color.LightGray;
+                    item.BackColor = Color.Black;
+                }
+                else if ((f.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                    color = Color.Green;
+
+                item.ForeColor = color;
+
+                listFiles.Items.Add(item);
+            }
+
+            FileInfo[] fileNames = unidad.RootDirectory.GetFiles("*.*");
+            foreach (FileInfo f in fileNames)
+            {
+                color = Color.Black;
+                item = new ListViewItem(f.Name);
+                item.SubItems.Add(f.Attributes.ToString());
+                if ((f.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                    color = Color.Green;
+                item.ForeColor = color;
+                listFiles.Items.Add(item);
+                //listFiles.Items.Add(f.Name + " " + f.LastAccessTime + " " + f.Length);
+            }
+
+            gbArchivos.Text = "Contendio de la unidad " + unidad.ToString();
+        }
+
+
+
+        private void mBloqueaNoExtraibles_Click(object sender, EventArgs e)
+        {
+            if (bloqueaHD)
+            {
+                DialogResult dialogResult = MessageBox.Show(
+                    "Estas a punto de desbloquear unidades NO EXTRAIBLES\n" +
+                    "como el disco duro de tu equipo, debes comprender\n" +
+                    "completamente los riesgos que se pueden provocar\n\n¿Deseas continuar?",
+                    "ADVERTENCIA",
+                    MessageBoxButtons.YesNo);
+
+                if (dialogResult == DialogResult.No)
+                    return;
+            }
+
+            string msj = "Las unidades no extraibles fueron ";
+            bloqueaHD = !bloqueaHD;
+            mBloqueaNoExtraibles.Checked = bloqueaHD;
+
+            msj += (bloqueaHD) ? "bloqueadas" : "desbloqueadas";
+            Consola(msj);
+        }
+
+        private void aumentarLimiteLineasTerminal(object sender, EventArgs e)
+        {
+
+            switch (sender.ToString())
+            {
+                case "25 lineas":
+                    limiteLineasConsola = 25;
+                    mLimite50.Checked = mLimite100.Checked = false;
+                    break;
+                case "50 lineas":
+                    limiteLineasConsola = 50;
+                    mLimite25.Checked = mLimite100.Checked = false;
+                    break;
+                case "100 lineas":
+                    limiteLineasConsola = 100;
+                    mLimite25.Checked = mLimite50.Checked = false;
+                    break;
+            }
+            Consola("cambiando a " + sender.ToString());
+        }
+
+        private void btnRecuperar_Click(object sender, EventArgs e)
+        {
+            String res = ejecutarComando("attrib  -r -a -s -h " + cmbDrives.Text + "/*.* /s /d");
+            Consola(res);
+        }
+
+        private void btnMostrarArchivos_Click(object sender, EventArgs e)
+        {
+            if (cmbDrives.Text != string.Empty)
+            {
+                buscaArchivos(cmbDrives.Text);
+            }
+            else
+            {
+                MessageBox.Show("Debes seleccionar una unidad");
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            gbAccion.Enabled = false;
+            Consola("Actualizando lista de dispositivos");
+            buscaUnidadesDrive();
+            gbAccion.Enabled = true;
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(".:|:. Darth Leonard .:|:.\nleolinuxmx@gmail.com\n@copyleft", "Acerca de este software");
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
